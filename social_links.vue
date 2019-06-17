2122894426f87d57934d8b72f1524c19f8c71143
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-17 16:26:21 -0400

    Sevenoaks 1.0  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <a href="https://www.facebook.com/shopsevenoaks" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
+        <a href="https://twitter.com/shopsevenoaks" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+        <a href="https://www.instagram.com/shopsevenoaks/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+    </div>
+    
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
