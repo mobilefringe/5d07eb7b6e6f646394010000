6b804ddf6e621bdb02992b9154f914fb782cacd2
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-17 16:00:26 -0400

    Sevenoaks 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -24,7 +24,7 @@ define([], function() {
             component: view('default'),
             children: [{
                     path: '',
-                    component: view('promotionsdddd'),
+                    component: view('promotions'),
                     name: 'promoList'
                 },
                 {
