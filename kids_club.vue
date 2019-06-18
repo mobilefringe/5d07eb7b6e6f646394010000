42194c72ef394df11f33a6b548f79731e3fc925e
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-18 13:21:46 -0400

    Sevenoaks 1.0  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -11,6 +11,15 @@
             <div class="row text-left">
                 <div class="col-sm-12">
                     <div v-if="currentPage" v-html="currentPage.body"></div>
+                    <div>
+                        <div class="details_img_container">
+                            <img id="about_page_image" src="" alt="">
+                        </div>
+                        <div class="details_info_container">
+                            <div class="details_title" id="about_page_title"></div>
+                            <div class="details_info" id="about_page_desc"></div>
+                        </div>
+                    </div>
                 </div>
             </div> 
             <div class="row"> 
