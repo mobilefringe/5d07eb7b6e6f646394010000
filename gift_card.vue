52b08838ba40bfea02956c4891659140cf7303dd
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-18 14:22:42 -0400

    Sevenoaks 1.0  /gift_card.vue

--- gift_card.vue
+++ gift_card.vue
@@ -141,7 +141,7 @@
             created() {
                 this.loadData().then(response => {
                     this.currentPage = response[0].data;
-                    this.currentContest = this.findContestBySlug('parklandmall-kids-club');
+                    // this.currentContest = this.findContestBySlug('parklandmall-kids-club');
                     var temp_repo = this.findRepoByName('Gift Card Banner');
                     if(temp_repo) {
                         this.pageBanner = temp_repo.images[0];
@@ -162,7 +162,7 @@
                     try {
                         // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
                         var host = this.property.mm_host.replace("http:", "");
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: host + "	/pages/sevenoaks-kids-club.json"}),this.$store.dispatch("getData", "contests"),this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: host + "	/pages/sevenoaks-gift-cards.json"}),this.$store.dispatch("getData", "contests"),this.$store.dispatch("getData", "repos")]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
