886e06d87dd273bd9c11de0ac7048dc0ceb88f2c
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-18 12:56:40 -0400

    Sevenoaks 1.0  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -31,13 +31,15 @@
                 				<label class="label" for="child_age">Child's Age <span class="req_star"> *</span></label>
                 				<input v-model="form_data.child_age" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="child_age" type="text" placeholder="Last Name" data-vv-delay="500" data-vv-as="child's age" id="child_age" required>
                 				<span v-show="errors.has('child_age')" class="form-control-feedback">{{ errors.first('child_age') }}</span>
-                				<p class="kids_club_form_notice">Happy Kids Club is for kids 10 years of age and under</p>
                 			</div>
                 			<!--<input class="form-control" name="cm-name" type="hidden" :value="form_data.child_first_name + ' ' + form_data.child_last_name">-->
                 			<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('birthday')}"   style="padding-top: 20px;">
                 				<label class="label" for="birthday">Child's Birthday <span class="req_star"> *</span></label>
                 				<input v-model="form_data.birthday" class="form-control" v-validate="'required|date_format:MM/DD/YYYY'" :class="{'input': true}" name="cm-f-fjutut" type="text" placeholder="mm/dd/yyyy" data-vv-delay="500" data-vv-as="birthday" id="birthday" required>
                 				<span v-show="errors.has('cm-f-fjutut')" class="form-control-feedback">{{ errors.first('cm-f-fjutut') }}</span>
+                			</div>
+                			<div class="col-sm-6 col-xs-12" style="padding-top: 20px;">
+                			    <p class="kids_club_form_notice">Happy Kids Club is for kids 10 years of age and under</p>
                 			</div>
                 		</div>
 						<div class="form-group">
