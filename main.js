9e8e64483ad664ad11998b67fb96dc91d219e7b7
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-18 13:31:42 -0400

    Sevenoaks 1.0  /main.js

--- main.js
+++ main.js
@@ -157,7 +157,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
             loadData: async function() {
                 try {
                     await this.$store.dispatch('initializeApi', {
-                        site: "sevenoaks",
+                        // site: "sevenoaks",
+                        site: "parklandmall",
                         version: "v4"
                     });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
