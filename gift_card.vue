21343e8d7b81cac7639eaee6d617f86a07a41819
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-18 14:56:10 -0400

    Sevenoaks 1.0  /gift_card.vue

--- gift_card.vue
+++ gift_card.vue
@@ -21,7 +21,7 @@
         Vue.use(Meta);
         Vue.use(VueLazyload);
         Vue.use(VuePaginate);
-        return Vue.component("gift-cards-component", {
+        return Vue.component("gift-cards", {
             template: template, // the variable template will be injected
             props:['locale'],
             data: function() {
