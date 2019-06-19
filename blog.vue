a4b243c03365efeaac5f46bf5592b3988165f2f2
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-19 16:22:15 -0400

    Sevenoaks 1.0  /blog.vue

--- blog.vue
+++ blog.vue
@@ -3,7 +3,7 @@
         <div class="margin_60"></div>
 		<div class="site_container inside_page_content page_content">
 		    <div class="margin_60 main_post_container clearfix" v-if="firstPost" v-for="post in firstPost">
-                <div class="post_image">
+                <div class="main_post_image">
                     <img :src="post.image_url" :alt="'Blog Post: ' + post.title">
                 </div>
                 <div class="main_post_content">
