d2232ae29c23af2cb1912430001ef74378682d9c
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-19 12:51:46 -0400

    Sevenoaks 1.0  /main.js

--- main.js
+++ main.js
@@ -89,8 +89,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 newsletter_email: "",
                 stickyMenu: false,
                 isSticky: 350,
-                subdomain: "sevenoaks",
-                twitter_user: "sevenoaks"
+                subdomain: "sudburycentre",
+                twitter_user: "newsudbury"
             }
         },
         created() {
