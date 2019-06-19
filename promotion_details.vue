6cd13766a1d1bafe9b21f9a6223ec060178779da
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-19 11:28:57 -0400

    Sevenoaks 1.0  /promotion_details.vue

---
+++ promotion_details.vue
@@ -0,0 +1,196 @@
+<template>
+	<div class="promo_dets_container" v-if="currentPromo">
+		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+			<!--http://via.placeholder.com/1920x300-->
+			<div class="site_container">
+				<div class="header_content caps">
+					<h1>{{$t("promos_page.promotions")}}</h1>
+					<h2 style="display:none;">Scroll to  view promotion details</h2>
+					<h3 style="display:none;">Scroll to  view promotion details</h3>
+				</div>
+			</div>
+		</div>
+		<div class="site_container">
+			<div class="row">
+				<div class="col-sm-4 promo_logo_container hidden_phone">
+					<!--<div class="image_container">-->
+					<!--	<img v-if="currentPromo.store" :src="currentPromo.store.store_front_url_abs" class="image" :alt="currentPromo.store.name+' Logo'"/>-->
+					<!--</div>-->
+					<div class="image_container details_store_image">
+						<!--<img v-lazy="currentStore.store_front_url_abs" class="image"/>-->
+						<div v-if="currentPromo.store.no_store_logo" class="store_details_image center-block">
+                            <div class="no_logo">
+                                <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
+                                <h2 class="store_details_name">{{ currentPromo.store.name }}</h2>
+                            </div>    
+                        </div>
+                        <img v-else class="store_details_image center-block" :src="currentPromo.store.store_front_url_abs" :alt="currentPromo.store.name + ' Logo'" />
+					</div>
+					<div class="text-center">
+					    <div v-if="currentPromo.promotionable_type == 'Store'">
+						    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{currentPromo.store.name}}</h4>
+						    <h4 class="event_store_name caps" v-else>{{currentPromo.store.name_2}}</h4>
+						</div>
+						<h4 v-if="currentPromo.store.phone" class="store_dets_title"> <a :href="'tel:'+currentPromo.store.phone">{{currentPromo.store.phone}}</a></h4>
+						<h4 v-if="currentPromo.store.website" class="store_dets_title"> <a :href="'//'+currentPromo.store.website" target="_blank"> {{$t("stores_page.store_website")}}</a></h4>
+						<h4 v-if="storeHours.length > 0" class="store_dets_title"> {{$t("stores_page.store_hours")}}</h4>
+						<ul class="store_hours_list">
+							<li v-if="storeHours" v-for="hour in storeHours" class="col-xs-12">
+								<span class="col-xs-6 text-left">{{hour.day_of_week | moment("dddd", timezone)}}</span>
+								<span class="col-xs-6 text-left">{{hour.open_time | moment("h A", timezone)}} - {{hour.close_time | moment("h A", timezone)}}</span>
+							</li>
+						</ul>
+						<div class="store_dets_btn caps" v-if="currentPromo.promotionable_type == 'Store'">
+							<router-link :to="'/stores/'+currentPromo.store.slug">{{$t("stores_page.store_dets_loc")}}</router-link>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-8 promo_image_container text-left">
+					<router-link to="/promotions"><i class="fa fa-angle-left"></i> &nbsp;{{$t("promos_page.back_to_promos")}}</router-link>
+					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-if="locale=='en-ca'">{{currentPromo.name}}</h3>
+					<h3 class="promo_name" style="margin: 20px auto 0px;"  v-else>{{currentPromo.name_2}}</h3>
+					<div class="row">
+    					<div v-if="currentPromo.promotionable_type == 'Store'" class="visible_phone">
+    					    <h4 class="event_store_name caps" v-if="locale=='fr-ca'">{{currentPromo.store.name_2}}</h4>
+    					    <h4 class="event_store_name caps" v-else>{{currentPromo.store.name}}</h4>
+    					</div>
+						<p class="promo_div_date pull-left">{{currentPromo.start_date | moment("MMM D", timezone)}} - {{currentPromo.end_date | moment("MMM D", timezone)}}</p>
+						<social-sharing :url="$root.shareURL('promotions',currentPromo.slug)" :title="currentPromo.title" :description="currentPromo.body" :quote="_.truncate(currentPromo.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="currentPromo.image_url" inline-template >
+							<div class="blog-social-share pull-right" style="margin: 15px auto;">
+								<div class="social_share">
+									<network network="facebook">
+										<i class="fa fa-facebook social_icons" aria-hidden="true"></i>
+									</network>
+									<network network="twitter">
+										<i class="fa fa-twitter social_icons" aria-hidden="true"></i>
+									</network>
+								</div>
+							</div>
+						</social-sharing>
+					</div>
+					<div class="col-sm-12 no_padding">
+						<img v-if="!_.includes(currentPromo.image_url, 'missing')" v-lazy="currentPromo.image_url" class="image" :alt="currentPromo.name"/>
+						<div class="text-left promo_description">
+							<p v-if="locale=='en-ca'" v-html="currentPromo.rich_description"></p>
+							<p v-else v-html="currentPromo.rich_description_2"></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    define(['Vue', 'vuex', 'moment', 'vue-lazy-load'], function(Vue, Vuex, moment, VueLazyload) {
+        Vue.use(VueLazyload);
+        return Vue.component("promo-details-component", {
+            template: template, // the variable template will be injected,
+            props:['id', 'locale'],
+            data: function() {
+                return {
+                    currentPromo: null,
+                    storePromos : null,
+                    storeHours : null,
+                    pageBanner : null
+                }
+            },
+            beforeRouteUpdate(to, from, next) {
+                this.currentPromo = this.findPromoBySlug(to.params.id);
+                    if (this.currentPromo === null || this.currentPromo === undefined){
+                        this.$router.replace('/');
+                    }
+                next();
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.updateCurrentPromo(this.id);
+                    var temp_repo = this.findRepoByName('Promotions Banner');
+                    if(temp_repo && temp_repo.images) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    else {
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "";
+                    }
+                    this.promos = this.promotions;
+                });
+            },
+            watch: {
+                currentPromo : function (){
+                    if(this.currentPromo != null) {
+                        if (this.currentPromo.store  && _.includes(this.currentPromo.store.store_front_url_abs, 'missing')) {
+                            // this.currentPromo.store.store_front_url_abs = this.property.default_logo_url;
+                            this.currentPromo.store.no_store_logo = true;
+                        }
+                        else if (!this.currentPromo.store) {
+                            this.currentPromo.store = {};
+                            this.currentPromo.store.store_front_url_abs = this.property.default_logo_url;
+                        }
+                        // if ( _.includes(this.currentStore.store_front_url_abs, 'missing')) {
+                        //   this.currentStore.no_store_logo = true;
+                        // } else {
+                        //   this.currentStore.no_store_logo = false;
+                        // }
+                        var vm = this;
+                        var temp_promo = [];
+                        var current_id =_.toNumber(this.currentPromo.id);
+                        _.forEach(this.currentPromo.store.promotions, function(value, key) {
+                            if(_.toNumber(value) != current_id){
+                                var current_promo = vm.findPromoById(value);
+                                current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
+                                temp_promo.push(current_promo);
+                            }
+                        });
+                        this.storePromos = temp_promo;
+                    }
+                    if(this.currentPromo.store) {
+                        var storeHours = [];
+                        var vm = this;
+                        _.forEach(this.currentPromo.store.store_hours, function (value, key) {
+                            var hour = vm.findHourById(value);
+                            if(hour.day_of_week === 0){
+                                hour.order = 7;
+                            }
+                            else {
+                                hour.order = hour.day_of_week;
+                            }
+                            storeHours.push();
+                        });
+                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedPromos',
+                    'findPromoBySlug',
+                    'findPromoById',
+                    'timezone',
+                    'findRepoByName',
+                    'findHourById'
+                ]),
+                allPromos() {
+                    return this.processedPromos;
+                },
+            },
+            methods: {
+                updateCurrentPromo (id) {
+                    this.currentPromo = this.findPromoBySlug(id);
+                    if (this.currentPromo === null || this.currentPromo === undefined){
+                        this.$router.replace('/');
+                    }
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+    });
+</script>
