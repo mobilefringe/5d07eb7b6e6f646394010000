fc87f953bb6e1279286fc723fa1cd2538d633d7d
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-19 14:39:09 -0400

    Sevenoaks 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -17,8 +17,8 @@
                                 <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name" id="fieldName">
                             </div>
                             <div class="col-sm-6 col-xs-12">
-                                <label class="label" for="newsletter_email">{{$t("newsletter_page.email")}}</label>
-                                <input v-model="form_data.email" required class="form-control js-cm-email-input" name="cm-wtdlhj-wtdlhj" type="email" placeholder="Email" id="newsletter_email">
+                                <label class="label" for="fieldEmail">{{$t("newsletter_page.email")}}</label>
+                                <input v-model="form_data.email" required class="form-control js-cm-email-input" name="cm-ydjkad-ydjkad" type="email" placeholder="Email" id="fieldEmail">
                             </div>
                         </div>
                         <div class="form-group">
@@ -31,7 +31,7 @@
     					</div>
     					<div class="form-group">
                             <div class="col-xs-12">
-                                <button class="contest_btn" type="submit" :disabled="formSuccess">{{$t("newsletter_page.subscribe")}}</button>
+                                <button class="contest_btn js-cm-submit-button" type="submit" :disabled="formSuccess">{{$t("newsletter_page.subscribe")}}</button>
                             </div>
                         </div>
                     </form>
