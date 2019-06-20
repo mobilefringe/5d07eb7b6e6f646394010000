00fea936e9527630b5ca18528472346b35c0bc93
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 11:36:16 -0400

    Sevenoaks 1.0  /guest_services.vue

--- guest_services.vue
+++ guest_services.vue
@@ -73,7 +73,7 @@
                 loadData: async function(id) {
                     try {
                         // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/" + this.id + ".json"}),this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/sevenoaks-guest-services".json"}),this.$store.dispatch("getData", "repos")]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
