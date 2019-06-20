2da3ec50c214c473086b04daacd278df79721871
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 09:06:47 -0400

    Sevenoaks 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -34,7 +34,7 @@
                         </div>
                         <div class="padding_top_20"></div>
                         
-                        <h3 class="hours_heading text-left">{{$t("hours_page.holiday_closures")}}</h3></span>
+                        <span v-if="holiday_closures"><h3 class="hours_heading text-left">{{$t("hours_page.holiday_closures")}}</h3></span>
                         <div id="closed_hours_container" class="hours_container">
                             <div class="hours_div text-left" v-for="hour in closeHolidays">
                                 <span>
