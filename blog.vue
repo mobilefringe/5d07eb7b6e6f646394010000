e0be9e834fd804a054259347002e7045d254a2ec
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 08:52:33 -0400

    Sevenoaks 1.0  /blog.vue

--- blog.vue
+++ blog.vue
@@ -34,7 +34,7 @@
 		<!--			</social-sharing>-->
   <!--              </div>-->
   <!--          </div>-->
-            <div class="post_container" >
+            <div class="post_container" v-if="posts" v-for="post in posts">
                 <div class="post_image">
                     <img :src="post.image_url" :alt="'Blog Post: ' + post.title">
                 </div>
