3fc4cc6a0d4124b27d27e00eea534d42b5f3d9a1
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 09:58:36 -0400

    Sevenoaks 1.0  /guest_services.vue

--- guest_services.vue
+++ guest_services.vue
@@ -6,7 +6,7 @@
                 <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
         			<div class="site_container">
         				<div class="header_content">
-        					<h1>Gift Cards</h1>
+        					<h1>Guest Services</h1>
         				</div>
         			</div>
         		</div>
