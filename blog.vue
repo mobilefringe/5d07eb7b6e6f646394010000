1e43037f34f426e76674ba264168c8eed133f884
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 10:28:35 -0400

    Sevenoaks 1.0  /blog.vue

--- blog.vue
+++ blog.vue
@@ -142,7 +142,7 @@
                 loadData: async function () {
                     try {
                         let results = await Promise.all([this.$store.dispatch("getData", "blogs")]);
-                        console.log(results, "res")
+                        console.log(getData, "res")
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
