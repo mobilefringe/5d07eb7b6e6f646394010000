c57f18513629676c888905b42087e4b2b88302fa
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-20 09:08:58 -0400

    Sevenoaks 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -21,7 +21,7 @@
                         </div>
                     </div>
                     <div class="col-sm-6 ">
-                        <span v-if="holiday_name"><h3 class="hours_heading text-left">{{$t("hours_page.holiday_hours")}}</h3></span>
+                        <span v-if="hours_page.holiday_hours"><h3 class="hours_heading text-left">{{$t("hours_page.holiday_hours")}}</h3></span>
                         <div id="holidays_hours_container" class="hours_container">
                             <div class="hours_div text-left" v-for="hour in reducedHolidays">
                                 <span>
