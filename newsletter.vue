3fccad6fda0cbb90fe8b72770051b5b8022b9bf8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-06-26 13:50:37 -0400

    Sevenoaks 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -14,7 +14,7 @@
                     <div class="text-left contact_us_body" v-if="currentPage.body" v-html="currentPage.body"></div>
                 </div>
                 <div class="col-md-12 contact_contents">
-                    <form class="form-horizontal js-cm-form" id="subForm" class="js-cm-form" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1AEC652151923F368AFF8F79BD97653D518B1251FC5BB09D7603C4AFEECA699B380141E6B93F1A28592DA91D0CB25CE7F2">
+                    <form class="form-horizontal js-cm-form" id="subForm" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1AEC652151923F368AFF8F79BD97653D518B1251FC5BB09D7603C4AFEECA699B380141E6B93F1A28592DA91D0CB25CE7F2">
                         <div class="form-group ">
                             <div class="col-sm-6 col-xs-12" >
                                 <label class="label" for="fieldName">{{$t("newsletter_page.name")}}</label>
