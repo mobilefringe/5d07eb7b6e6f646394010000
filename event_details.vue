79f57d57c5fc33170d52bff7333f00c5ff12b923
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-02 14:16:24 -0400

    Sevenoaks 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -31,12 +31,10 @@
 							</div>
 						</social-sharing>
 					</div>
-					<div class="col-sm-12 no_padding text-center">
-						<img v-if="!_.includes(currentEvent.image_url, 'missing')" v-lazy="currentEvent.image_url" class="image" :alt="currentEvent.name"/>
-						<div class="text-left promo_description">
-							<p v-if="locale=='en-ca'" v-html="currentEvent.rich_description"></p>
-							<p v-else v-html="currentEvent.rich_description_2"></p>
-						</div>
+					<img v-if="!_.includes(currentEvent.image_url, 'missing')" v-lazy="currentEvent.image_url" class="image" :alt="currentEvent.name"/>
+					<div class="text-left promo_description">
+						<p v-if="locale=='en-ca'" v-html="currentEvent.rich_description"></p>
+						<p v-else v-html="currentEvent.rich_description_2"></p>
 					</div>
 				</div>
 			</div>
