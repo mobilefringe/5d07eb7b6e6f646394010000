604b3e16b77ab2fbfc3513a6458b98750f90b215
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-02 12:22:48 -0400

    Sevenoaks 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -140,7 +140,7 @@
                             temp_promo.push(value);
                         }
                     });
-                    temp_promo = _.sortBy(temp_promo, ['created_at', 'end_date']).reverse();
+                    temp_promo = _.sortBy(temp_promo, ['end_date']);
                     return temp_promo;
                 },
             },
