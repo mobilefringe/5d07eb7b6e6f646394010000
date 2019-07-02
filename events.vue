a3aa9862aa325dda5a3b3531163c2b7a0795902c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-02 12:28:31 -0400

    Sevenoaks 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -153,7 +153,30 @@
                     return temp_promo;
                 },
                 contests() {
-                    
+                    var vm = this;
+                    var temp_promo = [];
+                    _.forEach(this.processedContests, function(value, key) {
+                        today = moment().tz(vm.timezone);
+                        webDate = moment(value.show_on_web_date).tz(vm.timezone);
+                        if (today >= webDate) {
+                            value.description_short = _.truncate(value.description, {
+                                'length': 150
+                            });
+                            value.description_short_2 = _.truncate(value.description_2, {
+                                'length': 150
+                            });
+                            if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
+                                value.store.store_front_url_abs = vm.property.default_logo_url;
+                            }
+                            else if (value.store == null || value.store == undefined) {
+                                value.store = {};
+                                value.store.store_front_url_abs =  vm.property.default_logo_url;
+                            }
+                            temp_promo.push(value);
+                        }
+                    });
+                    temp_promo = _.sortBy(temp_promo, ['end_date'])
+                    return temp_promo; 
                 }
             },
             methods: {
