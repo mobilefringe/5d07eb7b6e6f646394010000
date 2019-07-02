a1bd6c2cd5b8471eccbd2e02671c1d2ec221d53c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-02 12:10:44 -0400

    Sevenoaks 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -122,6 +122,7 @@
                     }
                     
                     this.itemList = _.concat(this.events, this.contests);
+                    
                     this.dataloaded = true;
                 });
             },
