489fb9868d22fe8f0328da5ebf7c6d4fad5350d6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-15 14:09:39 -0400

    Sevenoaks 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-if="dataloaded">
-		<div class="page_header" v-if="pageBanner" v-lazy:background-image="pageBanner.image_url">
+		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("promos_page.promotions")}}</h1>
