4309066246d0c034cbc2c03caa6d46ab5ec01716
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-16 16:42:20 -0400

    Sevenoaks 1.0  /contest_details.vue

--- contest_details.vue
+++ contest_details.vue
@@ -185,9 +185,6 @@
 </template>
 
 <style>
-    #contact_us_container .row{
-        margin-left:inherit;
-    }
 
     .form-group .form-control-feedback{
         font-size: 12px;
@@ -196,25 +193,7 @@
         top: initial;
         bottom: -27px;
     }
-    .popover-container input {
-        margin-top: 10px;
-        display: block;
-        width: 100%;
-        height: 34px;
-        padding: 6px 12px;
-        font-size: 14px;
-        line-height: 1.42857143;
-        color: #555;
-        background-color: #fff;
-        background-image: none;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-        box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-        -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-        -o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-        transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-    }
+    
     .agreement.form-group {
         margin:0;   
     }
