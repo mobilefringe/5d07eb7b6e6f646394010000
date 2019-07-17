2f1b3dffbe7c45eeec5734c2e4d4f4c51473be9d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-17 09:30:18 -0400

    Sevenoaks 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -122,23 +122,8 @@
                             let errors = this.errors;
                             this.validNumError = false;
                             
-                            //format contests data for MM
-                            var contest_entry = {};
-                            contest_entry.contest = this.form_data;
                             var vm = this;
-                            host_name = this.property.mm_host.replace("http:", "");
-                            var url = host_name + "/contests/" + this.currentContest.slug + "/create_js_entry";
-                            $.ajax({
-                                url: url,
-                                type: "POST",
-                                data: contest_entry,
-                                success: function(data) {
-                                    vm.formSuccess = true;
-                                },
-                                error: function(data){
-                                    vm.formError = true;
-                                }
-                            });
+                            
                         }
 
                     })
