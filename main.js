c07242803ebf65bc8d9303d02368ac9c4acf1966
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-17 12:12:40 -0400

    Sevenoaks 1.0  /main.js

--- main.js
+++ main.js
@@ -46,12 +46,11 @@ require.config({
         'campaignMonitor': 'https://js.createsend1.com/javascript/copypastesubscribeformlogic',
         'hammer' : 'https://codecloud.cdn.speedyrails.net/sites/59bac7db6e6f644f22ba0000/text/javascript/1484859750000/hammer.min',
         'mapplic-map' : 'https://preview-mmvue.codecloudapp.com/mapplic.vue?noext',
-        'postscribe': 'https://cdnjs.cloudflare.com/ajax/libs/postscribe/2.0.6/postscribe.min',
-        'constantContact' : 'https://static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min'
+        // 'constantContact' : 'https://static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min'
     }
 });
 
-require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vue-i18n', 'locales','jquery','vue-meta', 'moment', 'moment-timezone', 'vue-moment','vue-social-sharing','vue-select', 'vue!search-component', 'vue-simple-spinner','vue!loader.vue','vue!social_links.vue', 'vue!header.vue', 'json!menu_items.json', 'constantContact'], function (Vue, Vuex, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, $, Meta, moment, tz, VueMoment, SocialSharing, VueSelect, SearchComponent, Spinner, Loader, SocialLinks, Header, MenuItems, constantContact) {
+require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vue-i18n', 'locales','jquery','vue-meta', 'moment', 'moment-timezone', 'vue-moment','vue-social-sharing','vue-select', 'vue!search-component', 'vue-simple-spinner','vue!loader.vue','vue!social_links.vue', 'vue!header.vue', 'json!menu_items.json'], function (Vue, Vuex, Vue2Filters, VueRouter, appRoutes, store, VueI18n, messages, $, Meta, moment, tz, VueMoment, SocialSharing, VueSelect, SearchComponent, Spinner, Loader, SocialLinks, Header, MenuItems) {
     Vue.use(Meta);
     Vue.use(VueRouter);
     Vue.use(Vue2Filters);
