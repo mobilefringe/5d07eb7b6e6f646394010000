fb13d00964954a919e57be7f2d39b253245a2477
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-07-17 12:29:26 -0400

    Sevenoaks 1.0  /contest_details.vue

--- contest_details.vue
+++ contest_details.vue
@@ -288,16 +288,7 @@
                     this.dataLoaded = true;
                 });
             },
-            updated(){
-                var self = this;
-                var script = document.createElement("script");
             
-                script.async = true;
-                script.defer = true;
-                script.src = "//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js";
-                // document.getElementById('cc_universal_code').appendChild(script);
-                document.getElementsByTagName("head")[0].appendChild(script);
-            },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
