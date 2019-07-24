7da2d4a9d2b8e176e111d6492d17be67c60eccd8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-24 16:32:33 -0400

    Sevenoaks 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -44,11 +44,11 @@
                         </div>
                     </form>
                     
-                    <!--<div id="send_contact_success" class="alert alert-success text-left" role="alert" v-show="formSuccess">-->
-                    <!--    <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>-->
-                    <!--    <span class="sr-only">Success</span>-->
-                    <!--    Thank you for contacting us. A member from our team will contact you shortly.-->
-                    <!--</div>-->
+                    <div id="send_contact_success" class="alert alert-success text-left" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
                     <!--<div id="send_contact_error" class="alert alert-danger text-left" role="alert" v-show="formError">-->
                     <!--    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>-->
                     <!--    <span class="sr-only">Error:</span>-->
