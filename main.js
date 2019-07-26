9d32660d46b632bb6e9d3a92aa6e97402c9fb8bc
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-26 15:04:03 -0400

    Sevenoaks 1.0  /main.js

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                 dataLoaded: false,
                 show_mobile_menu: false,
                 property_logo: "//codecloud.cdn.speedyrails.net/sites/5d07eb7b6e6f646394010000/image/png/1564002270000/SevenOaksWhite.png",
-                logo_dark: "//codecloud.cdn.speedyrails.net/sites/5d07eb7b6e6f646394010000/image/png/1564002270000/SevenOaksBlack.png",
+                logo_dark: "//codecloud.cdn.speedyrails.net/sites/5d07eb7b6e6f646394010000/image/png/1564167641000/SevenOaksBlack.png",
                 menu_items: MenuItems,
                 search_result : null,
                 suggestionAttribute: 'name',
