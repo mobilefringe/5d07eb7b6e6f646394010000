851d55deda80885bc49e6e98ad3451e9c586ac99
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-20 12:54:54 -0500

    Sevenoaks Shopping Centre 1.1  /map.vue

---
+++ map.vue
@@ -0,0 +1,154 @@
+<template>
+	<div v-if="dataloaded">
+	    <div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+			<div class="site_container">
+				<div class="header_content">
+					<h1>Centre Map</h1>
+				</div>
+			</div>
+		</div>
+		<div class="map_container site_container clearfix">
+			<div class="col-sm-4">
+                <div class="map_directory text-center hidden_phone">
+                    <h2 style="display:none;" class="map_title caps">Find a Store</h2>
+                    <h3 class="map_title caps">Find a Store</h3>
+                    <div id="stores_container" class="directory_list text-left">
+                        <li v-for="store in filteredStores" class="pointer">
+							<p class="directory_store_name caps" v-on:click="dropPin(store)">{{store.name}}</p>
+						</li>
+                    </div>
+                </div>
+                <div class="visible_phone">
+                    <p class="text_left">Find Store :</p>
+					<div class="alphabet-dd visible_phone" >
+					    <v-select :options="allStores" label="name" :searchable="false" :on-change="dropPin" id="mobile_alpha_list" placeholder="Select a Store"></v-select>
+				    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="map light_border">
+    				<mapplic-map ref="mapplic_ref" :height="650" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="mapStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-map>
+    			</div>
+            </div>
+		</div>
+	</div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue-select", "vue!mapplic-map"], function(Vue, Vuex, VueSelect, MapplicComponent) {
+        return Vue.component("stores-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    listMode: "alphabetical",
+                    selectedCat: null,
+                    selectedAlpha: "All",
+                    alphabet: ["All", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+                    filteredStores: null,
+                    dataloaded: false,
+                    mobile_store: false,
+                    windowWidth: 0,
+                    storeBanner : null,
+                    search_result : null,
+                }
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataloaded = true;
+                    this.filteredStores = this.allStores;
+
+                    var temp_repo = this.findRepoByName('Map Banner');
+                    if(temp_repo && temp_repo.images) {
+                        this.pageBanner = temp_repo.images[0];
+                        console.log(this.pageBanner, "hh")
+                    }
+                    else {
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "";
+                    }
+                });
+            },
+            watch: {
+                windowWidth: function() {
+                    if (this.windowWidth <= 768) {
+                        this.mobile_store = true;
+                    } else {
+                        this.mobile_store = false;
+                    }
+                },
+            },
+            mounted() {
+                this.$nextTick(function() {
+                    window.addEventListener('resize', this.getWindowWidth);
+                    //Init
+                    this.getWindowWidth();
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'processedStores',
+                    'findRepoByName'
+                ]),
+                allStores() {
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                    });
+                    return all_stores;
+                },
+                mapStores() {
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                        if(value.svgmap_region == null){
+                            value.svgmap_region = value.id;
+                        }
+                    });
+                    return all_stores;
+                },
+                storeNames () {
+                    return _.map(this.processedStores, 'name');
+                },
+                getSVGMap(){
+                  return "//mallmaverick.com"+this.property.svgmap_url;  
+                },
+                floorList () {
+                    var floor_list = [];
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getSVGMap;
+                    floor_1.z_index = null;
+                    floor_1.show = true;
+                    floor_list.push(floor_1);
+                    
+                    return floor_list;
+                }
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                dropPin(store) {
+                    this.$refs.mapplic_ref.showLocation(store.svgmap_region);
+                },
+                getWindowWidth(event) {
+                    this.windowWidth = window.innerWidth;
+                },
+                onOptionSelect(option) {
+                    this.search_result = "";
+                    this.dropPin(option);
+                },
+            },
+            beforeDestroy: function() {
+                window.removeEventListener('resize', this.getWindowWidth);
+            },
+        });
+    });
+</script>
