807367b79d368f7af98946f368753320d70876ae
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-20 13:10:32 -0500

    Sevenoaks Shopping Centre 1.1  /feature_masonry.vue

---
+++ feature_masonry.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-masonry transition-duration="0.3s" item-selector=".grid-item" >
+        <div v-masonry-tile class="item feature_item_container" >
+            <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">
+                <router-link v-if="feature.url" :to="feature.url">
+                	<div :class="{ 'gallery-image' : feature.no_hover_class}"> 
+                	    <img :src="feature.image_url" :alt="'click to learn more about ' +feature.name">
+        				<div class="figcaption">
+        					<h3 class="hvr-underline-from-center">{{ feature.name }}</h3>
+        				</div>
+                	</div>
+            	</router-link>
+            	<div v-else>
+            	    <div :class="{ 'gallery-image' : feature.no_hover_class}"> 
+                	    <img :src="feature.image_url" :alt="'click to learn more about ' +feature.name">
+        				<div class="figcaption">
+        					<h3>{{ feature.name }}</h3>
+        				</div>
+                	</div>
+        		</div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex",  'masonry' , 'vue-masonry-plugin'], function(Vue, Vuex, masonry, VueMasonryPlugin) {
+        Vue.use(VueMasonryPlugin.default);
+        return Vue.component("feature-masonry", {
+            template: template, // the variable template will be injected,
+            props:['feature_items']
+        });
+    });
+</script>
