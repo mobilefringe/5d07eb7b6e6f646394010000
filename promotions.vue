4433cff116812393e4fb9d0347e5a34dcec60192
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-20 12:36:07 -0500

    Sevenoaks Shopping Centre 1.1  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -24,20 +24,16 @@
                         </div>
 					</div>
 					<div class="col-sm-6 col-md-8 event_dets_container">
-						<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
-						<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
+						<h4 class="event_name caps">{{promo.name}}</h4>
 						<div v-if="promo.promotionable_type == 'Store'">
-						    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
-						    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>
+						    <h4 class="event_store_name caps">{{promo.store.name}}</h4>
 						</div>
 						<p class="event_dates" v-if="isMultiDay(promo)">{{ promo.start_date | moment("MMMM D", timezone)}} - {{ promo.end_date | moment("MMMM D", timezone)}}</p>
                         <p class="event_dates" v-else>{{ promo.start_date | moment("MMMM D", timezone)}}</p>
-						<p class="event_desc"  v-if="locale=='en-ca'" >{{promo.description_short}}</p>
-						<p class="event_desc" v-else>{{promo.description_short_2}}</p>
-					
+						<p class="event_desc">{{promo.description_short}}</p>
 						<div class="text-right promo_link" v-if="promo">
 							<router-link :to="'/promotions/'+ promo.slug" class="event_learn_more pull-left"  :aria="promo.name">
-							   {{$t("promos_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>
+							   View Promotion Details <i class="fa fa-angle-right" aria-hidden="true"></i>
 						    </router-link>
 							<social-sharing :url="$root.shareURL('promotions',promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="promo.image_url" inline-template >
 								<div class="blog-social-share pull_right">
@@ -60,11 +56,11 @@
 			</div>
 			<div id="no_events" class="row" v-else>
 				<div class="col-md-12">
-					<p>{{$t("promos_page.no_promo_message")}}</p>
+					<p>There are no Promotions posted at this time. Please check back soon!</p>
 				</div>
 			</div>
 			<div class="show_more"  v-if="promos && showMore < promos.length">
-                <div class="contact_btn subscribe_btn pointer" @click="loadMoreItems()">{{$t("promos_page.more_promos")}}</div>
+                <div class="contact_btn subscribe_btn pointer" @click="loadMoreItems()">Load More</div>
             </div>
 		</div>
 	</div>
@@ -74,7 +70,6 @@
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
         return Vue.component("promos-component", {
             template: template, // the variable template will be injected
-            props:['locale'],
             data: function() {
                 return {
                     selectedDate: null,
@@ -117,24 +112,7 @@
                             value.description_short = _.truncate(value.description, {
                                 'length': 150
                             });
-                            value.description_short_2 = _.truncate(value.description_2, {
-                                'length': 150
-                            });
-                            // if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
-                            //     value.store.store_front_url_abs = vm.property.default_logo_url;
-                            // }
-                            // else if (value.store == null || value.store == undefined) {
-                            //     value.store = {};
-                            //     value.store.store_front_url_abs =  vm.property.default_logo_url;
-                            // }
-                            // if (value.store  && _.includes(value.store.store_front_url_abs, 'missing')) {
-                            //     // this.currentPromo.store.store_front_url_abs = this.property.default_logo_url;
-                            //     value.store.no_store_logo = true;
-                            // }
-                            // else if (!value.store) {
-                            //     value.store = {};
-                            //     value.store.store_front_url_abs = vm.property.default_logo_url;
-                            // }
+
                             temp_promo.push(value);
                         }
                     });
