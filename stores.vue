86d21bad3e66bb01f5b3110f7dcd240af3bf09b5
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-20 12:20:12 -0500

    Sevenoaks Shopping Centre 1.1  /stores.vue

---
+++ stores.vue
@@ -0,0 +1,165 @@
+<template>
+	<div v-if="dataloaded">
+		<div class="page_header" v-if="pageBanner" v-lazy:background-image="pageBanner.image_url">
+			<div class="site_container">
+				<div class="header_content">
+					<h1>Store Directory</h1>
+				</div>
+			</div>
+		</div>
+		<div class="site_container page_content">
+			<div class="row bold">
+				<div class="col-sm-6 col-md-4">
+					<div class="store_search" >
+						<search-component :list="allStores" placeholder="Find Your Store" suggestion-attribute="name" v-model="search_result" @select="onOptionSelect" class="text-left" :keys="['name']" :tokenize="true">
+							<template slot="item" scope="option" class="manual">
+								<article class="media">
+									<p>
+										<strong>{{ option.data.name }}</strong>
+									</p>
+								</article>
+							</template>
+						</search-component>
+						<i class="fa fa-search pull-right" id="store_search_img"></i>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="store_search" >
+						<div class="category-select-container">
+							<v-select v-model="selectedCat" :options="dropDownCats" :searchable="false" :on-change="filterByCategory" class="category-select" placeholder="Sort By Categories" id="selectByCat"></v-select>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12 hidden_phone">
+					<div class="store_search" >
+						<router-link class="directory_link" to="/map">
+							<div class="promotions_header_container directory_btn">View Map</div>
+						</router-link>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="store_list_container">
+				    <store-masonry :filteredStores="filteredStores"></store-masonry>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "vue-select",  "vue!search-component", "vue-lazy-load","vue!store-masonry"], function(Vue, Vuex, VueSelect, SearchComponent,VueLazyload, StoreMasonry) {
+        Vue.use(VueLazyload);
+        return Vue.component("stores-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    selectedCat: null,
+                    filteredStores: null,
+                    dataloaded: false,
+                    pageBanner : null,
+                    search_result : null,
+                }
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataloaded = true;
+                    this.filteredStores = this.allStores;
+                    
+                    var temp_repo = this.findRepoByName('Directory Banner');
+                    if(temp_repo && temp_repo.images) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    else {
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "";
+                    }
+                });
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                onOptionSelect(option) {
+                    this.search_result = "";
+                    this.$router.push("/stores/"+option.slug);
+                },
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'processedStores',
+                    'processedCategories',
+                    'storesByAlphaIndex',
+                    'storesByCategoryName',
+                    'findCategoryById',
+                    'findCategoryByName',
+                    'findRepoByName'
+
+                ]),
+                allStores() {
+                    var stores = this.processedStores;
+                    stores.map(store => {
+                        if (_.includes(store.store_front_url_abs, 'missing')) {
+                           store.no_store_logo = true;
+                        } else {
+                          store.no_store_logo = false;
+                        }
+                    });
+                    return this.processedStores;
+                },
+                allCatergories() {
+                    return this.processedCategories;
+                },
+                dropDownCats() {
+                    var cats = _.map(this.processedCategories, 'name');
+                    cats.unshift('All');
+                    return cats;
+                },
+                filterStores() {
+                    letter = this.selectedAlpha;
+                    if (letter == "All") {
+                        this.filteredStores = this.allStores;
+                    } else {
+                        var filtered = _.filter(this.allStores, function(o, i) {
+                            return _.lowerCase(o.name)[0] == _.lowerCase(letter);
+                        });
+                        this.filteredStores = filtered;
+                    }
+                },
+                filterByCategory() {
+                    category_id = this.selectedCat;
+                    if (category_id == "All" || category_id == null || category_id == undefined) {
+                        category_id = "All";
+                    } else {
+                        category_id = this.findCategoryByName(category_id).id;
+                    }
+
+                    if (category_id == "All") {
+                        this.filteredStores = this.allStores;
+                    } else {
+
+                        var find = this.findCategoryById;
+                        var filtered = _.filter(this.allStores, function(o) {
+                            return _.indexOf(o.categories, _.toNumber(category_id)) > -1;
+                        });
+                    
+                        this.filteredStores = filtered;
+                    }
+                    var el = document.getElementById("selectByCat");
+                    if(el) {
+                        el.classList.remove("open");
+                    }
+                    
+                },
+                
+            }
+        });
+    });
+</script>
