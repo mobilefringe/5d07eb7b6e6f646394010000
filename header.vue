c1e43ea35e13379986207185452e6bc163478dd6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-02-20 11:57:15 -0500

    Sevenoaks Shopping Centre 1.1  /header.vue

---
+++ header.vue
@@ -0,0 +1,272 @@
+<template>
+    <div class="fixed_menu"  :class="{'smallerHeader' : smallerHeader}">
+    	<div class="top_bar">
+    		<div class="site_container">
+    			<div class="row top_bar_wrapper">
+    				<div class="col-md-4 col-sm-6">
+    				    <div id="home_hours_container" v-if="todays_hours">
+    						<router-link to="hours" class="open_now">
+    						    <span v-if="todays_hours.is_closed == null || !todays_hours.is_closed ">Open Today</span>
+    						    <span v-else>Closed Today</span> 
+    						    <span v-if="todays_hours.is_closed == null || !todays_hours.is_closed ">
+    						    <span style="margin:0 20px">|</span> {{ todays_hours.open_time | moment("h:mma", timezone) }} - {{ todays_hours.close_time | moment("h:mma", timezone) }} </span>
+						    </router-link>
+    					</div>
+    					<div class="mobile_site_logo visible_phone">
+    					    <router-link to="/"><img :src="property_logo" :alt="property.name"/></router-link>
+    				    </div>
+    				</div>
+    				<div class="col-md-8 col-sm-6 hidden_phone text-right">
+    					<div class="header_social">
+        					<transition name="custom-classes-transition" enter-active-class="animated slideInRight" leave-active-class="animated slideOutRight">
+            					<div id="search_component_wrapper" class="inline_block" v-if="showSearch">
+            					    <div class="search_component_container" >
+                                        <search-component :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
+                                            <template slot="item" scope="option" class="manual">
+                                                <article class="media">
+                                                    <p>{{ option.data.name }}</p>
+                                                </article>
+                                            </template>
+                                        </search-component>
+                                    </div>
+            					</div>
+            				</transition>
+    					    <div class="search_icon inline_block solid_bg" @click="showSearch = !showSearch"> <i class="fa fa-search"></i></div>
+    					    <social-links class="inline_block solid_bg"></social-links>
+    					</div>
+    					<router-link id="signup" class="caps" to="/newsletter">Sign Up</router-link>
+    				</div>
+    				<div class="mobile_search_icon inline_block" @click="showMobileSearch = !showMobileSearch">
+    				    <i class="fa fa-search"></i>
+    				</div>
+    				<div id="menu-icon" @click="show_mobile_menu = !show_mobile_menu" :class="{ open: show_mobile_menu}">
+    					<span></span>
+    					<span></span>
+    					<span></span>
+    					<span></span>
+    				</div>
+    				<div class="mobile_nav_container visible_phone">
+    				    <transition name="custom-classes-transition" enter-active-class="animated slideInRight" leave-active-class="animated slideOutRight">
+    						<nav id="mobile_nav" v-show="show_mobile_menu">
+    							<ul>
+    								<div class="mobile_menu_site_logo">
+    									<router-link to="/">
+    									    <img v-if="property" src="//codecloud.cdn.speedyrails.net/sites/5e4eaf2b6e6f641683040000/image/png/1564167641000/SevenOaksBlack.png" :alt="property.name +' Mobile Logo'"/>
+									    </router-link>
+    								</div>
+    								<li v-for="(item,key) in menu_items" class="menu_item">
+        								<span @click="closeMobileMenu()" v-if="item.sub_menu == undefined">
+        								     <router-link :to="item.href">{{ item.name }}</router-link>
+        								</span>
+    							        <div v-else>
+    							            <b-card no-body class="mb-1">
+                                                <b-card-header header-tag="header" class="p-1" role="tab">
+                                                    <b-btn block @click="item.show_sub_menu = !item.show_sub_menu; toggleVar=!toggleVar;" :class="item.show_sub_menu ? 'collapsed' : null" :aria-controls="$t(item.name)" :aria-expanded="item.show_sub_menu ? 'true' : 'false'">
+                                                        {{ item.name }}
+                                                        <i v-if="item.show_sub_menu"  class="fa fa-minus"></i>
+                                                        <i v-else  class="fa fa-plus"></i>
+                                                    </b-btn>
+                                                </b-card-header>
+                                                <b-collapse v-model="item.show_sub_menu" :id="item.name" :visible="item.show_sub_menu" accordion="my-accordion" role="tabpanel" class="accordion_body">
+                                                    <b-card-body v-for="sub_menu in item.sub_menu">
+                                                        <p class="card-text" @click="closeMobileMenu()">
+                                                            <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank">{{ sub_menu.name }}</a>
+                                                            <a v-else-if="sub_menu.external_in_window" :href="sub_menu.href">{{ sub_menu.name }}</a>
+                    						                <router-link v-else-if="!sub_menu.router_name" :to="sub_menu.href">{{ sub_menu.name }}</router-link>
+                    						                <router-link v-else :to="{name : sub_menu.router_name, params: { new_store: sub_menu.prop }, query:{new_store: 'new_store'}} ">{{ sub_menu.name }}</router-link>
+                                                        </p>
+                                                    </b-card-body>
+                                                </b-collapse>
+                                            </b-card>
+    							        </div>
+    							    </li>
+    							</ul>
+    							
+    							<div class="small_hr"></div>
+    							<div class="tel_num" v-if="property">
+                                    <a v-if="property.contact_phone" :href="'tel:'+property.contact_phone">{{ property.contact_phone }}</a>
+                                </div>
+                                <div class="address" v-if="property">
+                                    <p> {{ property.address1 }}<br/>
+                                    {{ property.city }}, {{ property.province_state }} {{ property.postal_code }}</p>
+                                </div>
+    							<div class="header_social">
+    							    <social-links></social-links>
+    							</div>
+    							<span style="visibility: hidden;width: 0;height: 0;display: inline-block;">{{toggleVar}}</span>
+    							<div class="small_hr"></div>
+    						</nav>
+    					</transition>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	<transition name="custom-classes-transition" enter-active-class="animated slideInDown" leave-active-class="animated slideOutUp">
+			<div class="mobile_search_container col-xs-12" v-if="showMobileSearch">
+			    <search-component :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left inline_block col-xs-10">
+                    <template slot="item" scope="option" class="manual">
+                        <article class="media">
+                            <p>{{ option.data.name }}</p>
+                        </article>
+                    </template>
+                </search-component>
+                <i id="closeSiteSearch" class="closeSiteSearch fa fa-times col-xs-2" @click="showMobileSearch = false"></i>
+			</div>
+		</transition>
+    	<div class="menu_bar hidden_phone">
+    		<div class="site_container">
+    			<div class="nav_container hidden_phone">
+    				<div class="site_logo">
+    					<router-link to="/"><img :src="property_logo" :alt="property.name"/></router-link>
+    				</div>
+    				<div class="top_nav hidden_phone">
+    					<nav id="primary_nav">
+    						<ul>
+    						    <li v-for="(item, index) in menu_items" class="menu_item" :class="{dropdown : item.sub_menu,open : item.open_dropdown}" >
+                                    <router-link v-if="!item.sub_menu" :to="item.href">{{ item.name }}</router-link>
+    						        <span v-else to="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" :aria-expanded="item.open_dropdown">{{ item.name }}</span>
+    						        <ul v-if="item.sub_menu" class="dropdown-content">
+    						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
+    						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank">{{ sub_menu.name }}</a>
+    						                <a v-else-if="sub_menu.external_in_window" :href="sub_menu.href">{{ sub_menu.name }}</a>
+    						                <router-link v-else-if="!sub_menu.router_name" :to="sub_menu.href">{{ sub_menu.name }}</router-link>
+    						                <router-link v-else :to="{name : sub_menu.router_name, params: { new_store: sub_menu.prop }, query:{new_store: 'new_store'}} ">{{ sub_menu.name }}</router-link>
+    						            </li>
+    								</ul>
+    						    </li>
+    						</ul>
+    					</nav>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue", 'json!menu_items.json', 'jquery', 'vue!search-component'], function (Vue, Vuex, SocialLinks, BootstrapVue, MenuItems, $, SearchComponent) {
+        Vue.use(BootstrapVue);
+        return Vue.component("header-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    show_mobile_menu: false,
+                    property_logo: "//codecloud.cdn.speedyrails.net/sites/5e4eaf2b6e6f641683040000/image/png/1564167641000/SevenOaksWhite.png",
+                    menu_items: MenuItems,
+                    showSearch: true,
+                    search_result: null,
+                    suggestionAttribute: "name",
+                    keys: ["name", "description", "tags", "keywords", "store.name"],
+                    smallerHeader: false,
+                    isSticky: 200,
+                    toggleVar: false,
+                    showMobileSearch: false
+                }
+            },
+            watch: {
+                $route: function() {
+                    // hide dropdown when route changes
+                    _.forEach(this.menu_items, function(value, key) {
+                        value.show_sub_menu = false;
+                    });
+                    this.show_mobile_menu = false; //close menu when navigating to new page
+                },
+                show_mobile_menu: function() {
+                    if(this.show_mobile_menu === true){
+                        document.body.classList.add("no-scroll");
+                    } else if (this.show_mobile_menu === false) {
+                        document.body.classList.remove("no-scroll");
+                    }
+                }
+            },
+            created(){
+                window.addEventListener("scroll", this.makeNavbarSmall);
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'getTodayHours',
+                    'processedStores',
+                    'processedEvents',
+                    'processedPromos',
+                    'processedJobs'
+                ]),
+                searchList() {
+                    var events = this.processedEvents;
+                    _.forEach(events, function (value, key) {
+                        if (_.includes(value.eventable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });
+                    var promos = this.processedPromos;
+                    _.forEach(promos, function (value, key) {
+                        if (_.includes(value.promotionable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });s
+                    var jobs = this.processedJobs;
+                    _.forEach(jobs, function (value, key) {
+                        if (_.includes(value.jobable_type, 'Property')) {
+                            value.is_store = false;
+                        } else {
+                            value.is_store = true;    
+                        }
+                    });
+                    var stores = this.processedStores;
+                    _.forEach(stores, function (value, key) {
+                        value.is_store = true;    
+                    });
+                    
+                    var list = _.union( stores, events, promos, jobs );
+                    return list;
+                },
+                todays_hours() {
+                    return this.getTodayHours;
+                }
+            },
+            methods: {
+                toggleDropdown(index) {
+                    this.menu_items[index].open_dropdown = true;
+                },
+                onOptionSelect(option) {
+                    try{
+                        ga('send', 'event', 'Search Keywords', 'search', this.search_result);
+                    }
+                    catch (err){
+                        console.log("cannot send search data to GA tracking")
+                    }
+                    this.$router.push({
+                        name: "search-results",
+                        query: { searchQuery: this.search_result },
+                        params: { results: option }
+                    });
+                    this.$nextTick(function() {
+                        this.search_result = "";
+                        this.showMobileSearch = false;
+                    });
+                },
+                makeNavbarSmall(e) {
+                  if (window.pageYOffset >= this.isSticky) {
+                    this.smallerHeader = true;
+                  } else {
+                    this.smallerHeader = false;
+                  }
+                },
+                closeMobileMenu(){
+                    console.log("close menu")
+                    this.show_mobile_menu = false;
+                    return true;
+                }
+            },
+            destroyed: function() {
+                window.removeEventListener("scroll", this.makeNavbarSmall);
+            }
+        });
+    });
+</script>
