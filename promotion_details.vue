a4c1e63d7b4e2a05431928699635956698d9d124
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-04-15 11:51:34 -0400

    Sevenoaks Shopping Centre 1.1  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -18,7 +18,7 @@
 					<div class="image_container details_store_image">
 						<div v-if="currentPromo.store.no_store_logo" class="store_details_image center-block">
                             <div class="no_logo">
-                                <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
+                                <img class="store_img" src="//assets.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
                                 <h2 class="store_details_name">{{ currentPromo.store.name }}</h2>
                             </div>    
                         </div>
